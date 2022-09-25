3fd177a8c8ed20f77f1926ac2428e45fbe43d343
InstallB <33768114+InstallB@users.noreply.github.com>
2022-09-25 21:27:12 +0800

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,1 +1,18 @@
+import utility.Error;
+import utility.MxErrorListener;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import java.io.IOException;
+import java.io.InputStream;
 
+public class Compiler
+{
+    public static void main(String[] args) throws IOException
+    {
+        InputStream input_stream = System.in;
+        CharStream charstream = CharStreams.fromStream(input_stream);
+        throw new RuntimeException();
+    }
+}
